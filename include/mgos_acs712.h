48a3ab5e0fdb9842d767d60018d8de6f7406d20f
Apoorva Singh <apoorvasingh157@gmail.com>
2020-05-03 17:09:23 +0530

    Fixed all errors. Build successful!

--- include/mgos_acs712.h
+++ include/mgos_acs712.h
@@ -1,12 +1,19 @@
 #pragma once
 
+#ifdef __cplusplus
 extern "C" {
+#endif
+
+    #include <stdint.h>
 
     #define ADC_SCALE 1023.0
     #define VREF 5.0
     #define DEFAULT_FREQUENCY 50
 
     enum ACS712_type {ACS712_05B, ACS712_20A, ACS712_30A};
-    int mgos_acs712_setup(uint8_t _pin);
-    float mgos_acs712_get_current_dc(ACS712_type type, uint8_t _pin);
+    void mgos_acs712_setup(uint8_t _pin);
+    float mgos_acs712_get_current_dc(enum ACS712_type type, uint8_t _pin);
+
+#ifdef __cplusplus
 }
+#endif
--- src/mgos_acs712.c
+++ src/mgos_acs712.c
@@ -1,11 +1,12 @@
 #include "mgos_acs712.h"
 
 #include "mgos.h"
+#include "mgos_adc.h"
 
 int mgos_acs712_zero_point;
 
-float find_sensitivity(ACS712_type type) {
-  float sensitivity;
+float find_sensitivity(enum ACS712_type type) {
+  float sensitivity = 0.0;
   switch (type) {
 		case ACS712_05B:
 			sensitivity = 0.185;
@@ -20,7 +21,7 @@ float find_sensitivity(ACS712_type type) {
   return sensitivity;
 }
 
-void mgos_acs712_setup(int _pin) {
+void mgos_acs712_setup(uint8_t _pin) {
   mgos_adc_enable(_pin);
   uint16_t acc = 0;
 	for (int i = 0; i < 10; i++) {
@@ -30,8 +31,8 @@ void mgos_acs712_setup(int _pin) {
 }
 
 
-float mgos_acs712_get_current(ACS712_type type, uint8_t _pin) {
-  float sensitivity;
+float mgos_acs712_get_current(enum ACS712_type type, uint8_t _pin) {
+  float sensitivity = 0.0;
   switch (type) {
 		case ACS712_05B:
 			sensitivity = 0.185;
@@ -42,11 +43,11 @@ float mgos_acs712_get_current(ACS712_type type, uint8_t _pin) {
 		case ACS712_30A:
 			sensitivity = 0.066;
 			break;
-	}
+  }
 
   int16_t acc = 0;
 	for (int i = 0; i < 10; i++) {
-		acc += analogRead(_pin) - mgos_acs712_zero_point;
+		acc += mgos_adc_read(_pin) - mgos_acs712_zero_point;
 	}
 	// float I = (float)acc / 10.0 / ADC_SCALE * VREF / sensitivity;
   float I = (float)acc / 10.0 / 4095 * 3.3 / sensitivity;
