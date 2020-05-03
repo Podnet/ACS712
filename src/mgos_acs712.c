55bf85627075a7919e486e78572d1653b1ab7252
Apoorva Singh <apoorvasingh157@gmail.com>
2020-05-03 19:52:05 +0530

    More debugging statements

--- src/mgos_acs712.c
+++ src/mgos_acs712.c
@@ -30,10 +30,12 @@ float mgos_acs712_get_current(enum ACS712_type type, uint8_t _pin) {
 			break;
   }
   DBG(("ACS712 sensitivity: %f", sensitivity));
+  DBG(("ACS712 zero point: %d", mgos_acs712_zero_point));
   int16_t acc = 0;
 	for (int i = 0; i < 10; i++) {
 		acc += mgos_adc_read(_pin) - mgos_acs712_zero_point;
 	}
+  DBG(("AC712 acc: %d", acc));
 	// float I = (float)acc / 10.0 / ADC_SCALE * VREF / sensitivity;
   float I = (float)acc / 10.0 / 4095 * 3.3 / sensitivity;
   DBG(("ACS712 current: %f", I));
